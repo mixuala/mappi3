6b348c4894501cb99a1dc4f0a05fb119dfada30c
michael lin <mlininkl@gmail.com>
2018-12-06 16:16:15 +0800

    add CORS support for google cloud function, - use `https://us-central1-mappi3-c91f1.cloudfunctions.net/getOpenGraph?url=[url]`

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as suq from 'suq';
+import * as cors from 'cors';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
 
 const params = (request)=>{
   const q = request.url.split('?');
@@ -24,12 +25,39 @@ const params = (request)=>{
   return result;
 }
 
+
+const CORS_WHITELIST:string[] = ['http://localhost:8100', 'http://localhost:8101', 'https://mappi3-c91f1.firebaseapp.com'];
+
 export const getOpenGraph = functions.https.onRequest((request, response) => {
   request.params = params(request);
-  suq( request.params.url, (err, json, body)=>{
-    if (err) return response.send(err);
-    const openGraphTags = json.opengraph;
-    return response.send(openGraphTags);
-  })
+  console.log("querystring=", request.params);
+
+  const scrape = (url, res)=>{
+    return suq( url, (err, json, body)=>{
+      if (err) 
+        return res.send(err);
+
+      const openGraphTags = json.opengraph;
+      console.log("resp=", openGraphTags);
+      return res.send(openGraphTags);
+    });
+  };
+
+  const corsOptions = {
+    origin: true,
+    xxxorigin: (origin:string, cb)=>{
+      if (!origin || ~CORS_WHITELIST.indexOf(origin)) 
+        cb(null, true);
+      else cb(new Error("CORS no access allowed"));
+    }
+  };
+
+  const corsResp = cors(corsOptions);
+  return corsResp( request, response, ()=>{
+
+    scrape( request.params.url, response);
+
+  });
+
 });
 
