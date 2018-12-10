afce7636f1134e39873ee743a77958035dad15da
michael lin <mlininkl@gmail.com>
2018-12-10 10:25:41 +0800

    add isAppleNewsRedirect(), and fetch opengraph from redirect url

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,24 +28,88 @@ const params = (request)=>{
 
 const CORS_WHITELIST:string[] = ['http://localhost:8100', 'http://localhost:8101', 'https://mappi3-c91f1.firebaseapp.com'];
 
+/**
+ * warning: applenews redirects do NOT include og:url link
+ * // const testLink = "https://apple.news/AXmG-6cIzSM2i7mu24Mla-Q"
+ * compare: 
+ * - https://us-central1-mappi3-c91f1.cloudfunctions.net/getOpenGraph?url=https://apple.news/AE4ykQz9tRw6asIVKRVvxRQ
+ * - https://us-central1-mappi3-c91f1.cloudfunctions.net/getOpenGraph?url=https://www.nytimes.com/2018/12/07/science/climate-change-mass-extinction.html
+ * 
+ * check for http 304 redirect
+ */
 export const getOpenGraph = functions.https.onRequest((request, response) => {
   request.params = params(request);
-  console.log("querystring=", request.params);
+  // console.log("querystring=", request.params);
+  const requestOptions = {
+    // BUG: followRedirect seems to have no effect
+    // followRedirect:(resp)=>{
+    //   console.log("@@@ REDIRECT=", resp);
+    //   return true;
+    // },
+  };
 
+  /**
+   * Check if suq() response follows structure of an AppleNews redirect
+   *  json.tags.links[0] = {text:'Click here', href:[redirect url]}
+   * @param suqRes 
+   */
+  const isAppleNewsRedirect = (suqRes:any):string|boolean => {
+    try {
+      let redirect = suqRes.tags.links.find(o=>{ return o.text.toLowerCase()==='click here';});
+      if (!redirect){
+        // try again, look at hrefs
+        redirect = suqRes.tags.links.find(o=>{ return !o.href.startsWith('https://www.apple.com');});
+      }
+      if (redirect) {
+        // get redirect
+        // console.log("### AppleNews detected:", redirect.href);
+        return redirect.href;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    return false;
+  }
+
+  /**
+   * wrap suq() call inside CORS(corsOptions)( =>{ scrape() })
+   * @param url 
+   * @param res 
+   */
   const scrape = (url, res)=>{
-    return suq( url, (err, json, body)=>{
-      if (err) 
-        return res.send(err);
+    return suq( url
+      , (err, json, body)=>{
+        if (err) 
+          return res.send(err);
 
-      const openGraphTags = json.opengraph;
-      console.log("resp=", openGraphTags);
-      return res.send(openGraphTags);
-    });
+        const openGraphTags = json.opengraph || {};
+        // const images = json.tags && json.tags.images;
+        if (!!openGraphTags['og:url']){
+          // console.log("og:resp=", openGraphTags);
+          return res.send(openGraphTags);
+        }
+        else 
+        {
+          // console.log("NO OPENGRAPH TAGS, url=", url);
+          const redirectUrl = isAppleNewsRedirect(json);
+          if (redirectUrl) {
+            console.log( ">>> suq(), redirect=", redirectUrl);
+            return scrape( redirectUrl, res)
+          }
+          else {
+            // not an AppleNews suq() response, patch opengraph resp with original redirect url
+            openGraphTags['url'] = url;  // fallback url
+            return res.send(openGraphTags);
+          };
+        }
+        
+      }
+      , requestOptions);
   };
 
   const corsOptions = {
-    origin: true,
-    xxxorigin: (origin:string, cb)=>{
+    // origin: true,
+    origin: (origin:string, cb)=>{
       if (!origin || ~CORS_WHITELIST.indexOf(origin)) 
         cb(null, true);
       else cb(new Error("CORS no access allowed"));
